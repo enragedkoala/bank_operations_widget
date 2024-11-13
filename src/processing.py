11566fb53969cb59e885e0c6cbec6b9bb8d1f180
Pavel Tyunikov <paveltyunikov@yahoo.com>
2024-11-13 13:20:21 +0300

    lintered and corrected

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 def filter_by_state(list_of_operations: list, state_wanted: str = "EXECUTED") -> list:
-    """Function takes a list of operations and returns the list of ops with wanted state"""
+    """Function takes a list of bank operations and returns the list of ops with wanted state"""
     list_of_wanted_operations = []
     for operation in list_of_operations:
         if "state" not in operation.keys():
@@ -9,3 +9,13 @@ def filter_by_state(list_of_operations: list, state_wanted: str = "EXECUTED") ->
         if operation["state"] and operation["state"] == state_wanted:
             list_of_wanted_operations.append(operation)
     return list_of_wanted_operations
+
+
+def sort_by_date(list_of_operations: list, descending: bool = True) -> list:
+    """Function takes list of bank operations and returns the list sorted by date (descending by default)"""
+    for operation in list_of_operations:
+        if "date" not in operation.keys():
+            print("invalid input")
+            quit()
+    operations_sorted_by_date = sorted(list_of_operations, key=lambda operation: operation.get("date"), reverse=descending)
+    return operations_sorted_by_date
