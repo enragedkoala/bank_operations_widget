1e2d69b2f119cce6f77f50914db1c5d6e8c7137d
Pavel Tyunikov <paveltyunikov@yahoo.com>
2024-11-15 12:06:19 +0300

    test_processing. conftest refactor

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,13 @@
 def filter_by_state(list_of_operations: list, state_wanted: str = "EXECUTED") -> list:
     """Function takes a list of bank operations and returns the list of ops with wanted state"""
+    if not isinstance(list_of_operations, list):
+        print("Invalid input(data type)")
+        return []
     list_of_wanted_operations = []
     for operation in list_of_operations:
         if "state" not in operation.keys():
-            print("invalid input")
-            quit()
+            print("Invalid input(no state)")
+            return []
     for operation in list_of_operations:
         if operation["state"] and operation["state"] == state_wanted:
             list_of_wanted_operations.append(operation)
@@ -13,10 +16,13 @@ def filter_by_state(list_of_operations: list, state_wanted: str = "EXECUTED") ->
 
 def sort_by_date(list_of_operations: list, descending: bool = True) -> list:
     """Function takes list of bank operations and returns the list sorted by date (descending by default)"""
+    if not isinstance(list_of_operations, list):
+        print("Invalid input(data type)")
+        return []
     for operation in list_of_operations:
         if "date" not in operation.keys():
-            print("invalid input")
-            quit()
+            print("Invalid input(no date)")
+            return []
     operations_sorted_by_date = sorted(
         list_of_operations, key=lambda operation: operation.get("date"), reverse=descending
     )
