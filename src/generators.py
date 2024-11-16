c44df0cb5d644f593bd354a8c2a8c49d27677735
Pavel Tyunikov <paveltyunikov@yahoo.com>
2024-11-16 18:42:51 +0300

    transaction_descriptions and card_number_generator has been added

--- src/generators.py
+++ src/generators.py
@@ -12,9 +12,16 @@ def filter_by_currency(transactions: list, currency: str) -> Iterator:
             yield transaction
 
 
-def transaction_descriptions():
-    pass
+def transaction_descriptions(transactions: list) -> Iterator:
+    """Function takes list of dicts, representing operations and returns Iterator with operations descriptions"""
+    for transaction in transactions:
+        if "description" in transaction:
+            yield transaction["description"]
 
 
-def card_number_generator():
-    pass
+def card_number_generator(start: int, end: int) -> Iterator:
+    """Function generates split 16-digit card numbers in range (start, end+1)"""
+    for number in range(start, end + 1):
+        card_number = f"{number:016d}"
+        split_card_number = f"{card_number[:4]} {card_number[5:8]} {card_number[9:12]} {card_number[12:]}"
+        yield split_card_number
